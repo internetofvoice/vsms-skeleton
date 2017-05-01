980a88f14bc54035026a38ba56ec4be876a6ba48
Alexander Schmidt <anschluss80+git@mailbox.org>
2017-05-01 10:37:52 +0200

    Backports from skills project

--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -12,18 +12,17 @@ use \InternetOfVoice\VSMS\Core\Controller\AbstractController;
 final class HomeController extends AbstractController
 {
     /**
-     * Home method
+     * Home
      *
      * @param  \Slim\Http\Request   $request    Slim request
      * @param  \Slim\Http\Response  $response   Slim response
-     * @param  array                $args       Arguments
      * @return \Slim\Http\Response
      * @access public
      * @author a.schmidt@internet-of-voice.de
      */
-    public function home($request, $response, $args) {
+    public function home($request, $response) {
         return $this->container->get('renderer')->render($response, 'home/home.twig', [
-            'language'   => $this->i18n->getLanguage(),
+            'i18n'       => $this->i18n,
             'page_title' => 'Home',
             'hostname'   => $request->getUri()->getHost(),
         ]);
